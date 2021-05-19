dc9018dc206da1d59b604fff55810a36faa50183
davidallenbaer <davidallenbaer@gmail.com>
2021-05-18 23:07:57 -0400

    Add project files.

---
+++ DesignPattern_SingletonThreadSafe/Singleton.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern_SingletonThreadSafe
+{
+    public sealed class Singleton
+    {
+        private static int counter = 0;
+        private static readonly object Instancelock = new object();
+        private Singleton()
+        {
+            counter++;
+            Console.WriteLine("Counter Value " + counter.ToString());
+        }
+        private static Singleton instance = null;
+
+        public static Singleton GetInstance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (Instancelock)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new Singleton();
+                        }
+
+                    }
+                }
+                return instance;
+            }
+        }
+
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
